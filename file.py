748c90f6083872ea695ee6e081ce4a90893386e5
egg32 <63085965+egg32@users.noreply.github.com>
2020-04-02 20:36:51 -0700

    Update file.py

--- file.py
+++ file.py
@@ -1,2 +1,63 @@
-for x in range(1, 10):
-   print(x)
+import random
+
+
+sim_riddles = ['一月七日,猜一个字', '一加一,猜一字', '一半儿,猜一个字',
+'一字十三点,难在如何点,猜一个字', '一百减一,猜一个字', '一夜又一夜,猜一字',
+'一个人搬两个土,猜一个字', '一个礼拜,猜一个字', '一家十一口,猜一字',
+'一家有七口,种田种一亩,自己吃不够,还养一条狗', '一根木棍,吊个方箱,一把梯子,搭在中央',
+'一只牛,猜一个字', '一只狗四个口,猜一个字', '一一箭穿心,猜一字',
+'一点一横长,一撇到南洋,南洋有个人,只有一寸长', '一边是水,一边是山,猜一个字',
+'一边是红,一边是绿,一边喜风,一边喜雨', '七人八只眼,猜一个字', '七人头上长了草,猜一字',
+'七十二小时,猜一个字', '王先生白小姐坐在石头上', '九十九,猜一字', '九只鸟,猜一个字',
+'九号,猜一字', '九辆车,猜一个字', '四个人搬个木头,猜一个字', '一人,猜一个字',
+'一人一张口,口下长只手,猜一字', '一人在内,猜一字', '一人挑两小人,猜一字' ]
+Answers = ['脂', '王', '臼', '汁', '白', '多', '佳', '旨', '吉', '兽', '面', '生', '器', '必', '府', '汕', '秋', '货', '花', '晶',
+'碧', '白', '鸠', '旭', '轨', '杰', '大', '拿', '肉', '夹']
+
+
+while True:
+    print('如果你要猜謎語，案1。如果你要玩石頭剪子布，案2')
+    game = input()
+    print('')
+    print('--------------')
+    print('如果你要回去案b')
+    print('--------------')
+    print('')
+    if game == "1":
+        y = 0
+        print('這是個猜謎語的遊戲')
+        print('我們有30個謎語')
+        while True:
+            #ends game
+            if len(sim_riddles) == 0:
+                print('你都猜完了！')
+                break
+            if y == 1:
+                break
+
+
+
+            #selects the riddle number
+            riddle_num = random.randint(0, len(sim_riddles) - 1)
+            #selects the riddle based on the number .pop() for no repeats
+            riddle = sim_riddles.pop(riddle_num)
+            #next 2 lines is debug code
+            #print(riddle_num)
+            #print(Answers[riddle_num])
+            while True:
+                print(riddle + ':')
+                ans = input()
+                if ans == Answers[riddle_num]:
+                    print('你猜對了！')
+                    #makes sure that the riddle's index and the answer's index is the same
+                    del Answers[riddle_num]
+                    break
+                elif ans == 'b':
+                    y += 1
+                    break
+                else:
+                    print('你猜錯了！')
+   
+   
+   
+   
